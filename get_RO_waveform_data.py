02ca57a096079406ca5ddbd1c994b74dd2d2df59
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 14:15:01 +0200

    Update get_RO_waveform_data.py

--- get_RO_waveform_data.py
+++ get_RO_waveform_data.py
@@ -16,4 +16,4 @@ endtime = start + 60*60*12
 wave1 = client.get_waveforms('RO', 'MLR', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
 wave1.filter("lowpass", freq=0.1, corners=2)
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
-           one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='waveforms/RO/RO_12H_MLR_helicorder.png')
+           one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='waveforms/RO/RO_12H_MLR_HHZ.png')
