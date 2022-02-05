80271f0d29b6e9253e1cea908648e3d305b8ad60
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 20:23:16 +0200

    Update get_CJR_HHZ_waveform_data.py

--- get_CJR_HHZ_waveform_data.py
+++ get_CJR_HHZ_waveform_data.py
@@ -15,12 +15,12 @@ endtime = start + 60*60*12
 try:
  wave1 = NIEP_client.get_waveforms('RO', 'CJR', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
  #wave1.filter('lowpass', freq=0.2, corners=2, zerophase=False)
- wave1.filter("bandpass", freqmin=0.22, freqmax=25.00, corners=3, zerophase=False)
+ wave1.filter("bandpass", freqmin=0.12, freqmax=25.00, corners=3, zerophase=False)
  wave1.detrend(type='demean')
  wave1.detrend(type='linear')
  wave1.plot(type="dayplot", interval=30, size=(1360, 1060), dpi=164, number_of_ticks=6, tick_rotation=45,
         right_vertical_labels=False,
-        vertical_scaling_range=None, one_tick_per_line=True,
+        vertical_scaling_range=5e3, one_tick_per_line=True,
         color=['k', 'r', 'b', 'g'], show_y_UTC_label=True,
         outfile='public/waveforms/RO/RO_12H_CJR_HHZ.png')
 except Exception as e:
