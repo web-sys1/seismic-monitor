160c9285cf57638d2cd60dfce6a12af566515b0a
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 14:20:15 +0200

    Update get_RO_MLR_waveform_data.py

--- get_RO_MLR_waveform_data.py
+++ get_RO_MLR_waveform_data.py
@@ -3,8 +3,6 @@ from obspy import UTCDateTime
 
 import os
 
-os.makedirs('waveforms/RO')
-
 client = Client("NIEP")
 
 t1 = UTCDateTime.now()
@@ -16,4 +14,4 @@ endtime = start + 60*60*12
 wave1 = client.get_waveforms('RO', 'MLR', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
 wave1.filter("lowpass", freq=0.1, corners=2)
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
-           one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='waveforms/RO/RO_12H_MLR_HHZ.png')
+           one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_MLR_HHZ.png')
