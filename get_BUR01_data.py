2be1ab55d1530b25885874c0988e4ef5d493403d
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 18:34:04 +0200

    Update get_BUR01_data.py

--- get_BUR01_data.py
+++ get_BUR01_data.py
@@ -10,10 +10,12 @@ NIEP_client = Client("NIEP")
 t1 = UTCDateTime.now()
 
 start = t1 - 60*60*12
-
 endtime = start + 60*60*12
 
+
 wave1 = NIEP_client.get_waveforms('RO', 'BUR01', '--', 'HNZ', start.replace(minute=0, second=1), endtime)
-wave1.filter("lowpass", freq=0.1, corners=5)
+wave1.filter("bandpass", freqmin=0.22, freqmax=25.00, corners=3, zerophase=False)
+wave1.detrend(type='demean')
+wave1.detrend(type='linear')
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=True,
            one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_BUR01_HNZ.png')
