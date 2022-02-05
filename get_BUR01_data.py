70616b9fa20898cae5e8bd2eeb3974b07703fdef
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 14:58:48 +0200

    Update get_BUR01_data.py

--- get_BUR01_data.py
+++ get_BUR01_data.py
@@ -1,9 +1,11 @@
-
 from obspy.clients.fdsn import Client
 from obspy import UTCDateTime
-import os
+from obspy import read
+import matplotlib.pyplot as plt   # matplotlib and its pyplot sub-package is used for plotting
+import numpy as np                # numpy is used for numerical computing and with arrays
 
-client = Client("NIEP")
+
+NIEP_client = Client("NIEP")
 
 t1 = UTCDateTime.now()
 
@@ -11,7 +13,7 @@ start = t1 - 60*60*12
 
 endtime = start + 60*60*12
 
-wave1 = client.get_waveforms('RO', 'BUR01', '--', 'HNZ', start.replace(minute=0, second=1), endtime)
-wave1.filter("lowpass", freq=0.1, corners=2)
-wave1.plot(type='dayplot', interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
+wave1 = NIEP_client.get_waveforms('RO', 'BUR01', '--', 'HNZ', start.replace(minute=0, second=1), endtime)
+wave1.filter("lowpass", freq=0.1, corners=5)
+wave1.plot(type='dayplot', interval=60, right_vertical_labels=True,
            one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_BUR01_HNZ.png')
