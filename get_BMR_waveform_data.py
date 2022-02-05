3e855e30970447b73624a6fed40173880ad328ab
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 14:51:33 +0200

    Update get_BMR_waveform_data.py

--- get_BMR_waveform_data.py
+++ get_BMR_waveform_data.py
@@ -13,7 +13,7 @@ start = t1 - 60*60*12
 
 endtime = start + 60*60*12
 
-wave1.filter("lowpass", freq=0.1, corners=2)
 wave1 = NIEP_client.get_waveforms('RO', 'BMR', '--', 'BHZ', start.replace(minute=0, second=1), endtime)
+wave1.filter("lowpass", freq=0.1, corners=2)
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=True, vertical_scaling_range=3e3,
            one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=True, outfile='public/waveforms/RO/RO_12H_BMR_BHZ.png')
