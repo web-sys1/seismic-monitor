5ea576354e9781b1e759194c5d1d431d3f0d294c
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 18:29:33 +0200

    Update get_BZS_waveform_data.py

--- get_BZS_waveform_data.py
+++ get_BZS_waveform_data.py
@@ -1,17 +1,16 @@
 from obspy.clients.fdsn import Client
 from obspy import UTCDateTime
 from obspy import read
-import matplotlib.pyplot as plt   # matplotlib and its pyplot sub-package is used for plotting
-import numpy as np                # numpy is used for numerical computing and with arrays
+#import matplotlib.pyplot as plt   # matplotlib and its pyplot sub-package is used for plotting
+#import numpy as np                # numpy is used for numerical computing and with arrays
 
 
 NIEP_client = Client("NIEP")
 
 t1 = UTCDateTime.now()
 
-start = t1 - 60*60*17
-
-endtime = start + 60*60*17
+start = t1 - 60*60*14
+endtime = start + 60*60*14
 
 try:
  wave1 = NIEP_client.get_waveforms('RO', 'BZS', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
