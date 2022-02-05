8b6c589adb2dac810d56397873f4fd97b1f5abf8
Web Source (SRC) <web.system.management@gmail.com>
2022-02-05 14:26:17 +0200

    Update get_BUR01_data.py

--- get_BUR01_data.py
+++ get_BUR01_data.py
@@ -11,7 +11,7 @@ start = t1 - 60*60*12
 
 endtime = start + 60*60*12
 
-wave1 = client.get_waveforms('RO', 'BUR01', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
+wave1 = client.get_waveforms('RO', 'BUR01', '--', 'HNZ', start.replace(minute=0, second=1), endtime)
 wave1.filter("lowpass", freq=0.1, corners=2)
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
            one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_BUR01_BHZ.png')
