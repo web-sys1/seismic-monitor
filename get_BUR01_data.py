88f5a15444c7a51a8903742e07e8e59730573c9e
Web Source (SRC) <web.system.management@gmail.com>
2022-02-06 08:46:59 +0200

    Update get_BUR01_data.py

--- get_BUR01_data.py
+++ get_BUR01_data.py
@@ -18,4 +18,7 @@ wave1.filter("bandpass", freqmin=0.22, freqmax=25.00, corners=3, zerophase=False
 wave1.detrend(type='demean')
 wave1.detrend(type='linear')
 wave1.plot(type='dayplot', interval=60, right_vertical_labels=True,
-           one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_BUR01_HNZ.png')
+           vertical_scaling_range=1600,
+           one_tick_per_line=True,
+           color=['k', 'r', 'b', 'g'],
+           show_y_UTC_label=False, outfile='public/waveforms/RO/RO_12H_BUR01_HNZ.png')
