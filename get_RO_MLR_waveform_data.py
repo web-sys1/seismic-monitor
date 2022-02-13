4042271c9613f7d245e56d644e7c41f5011f04c6
Web Source (SRC) <web.system.management@gmail.com>
2022-02-13 14:39:41 +0200

    Update get_RO_MLR_waveform_data.py

--- get_RO_MLR_waveform_data.py
+++ get_RO_MLR_waveform_data.py
@@ -14,7 +14,7 @@ endtime = start + 60*60*12
 wave1 = NIEP_client.get_waveforms('RO', 'MLR', '--', 'HHZ', start.replace(minute=0, second=1), endtime)
 
 """
-wave1.filter("bandpass", freqmin=0.22, freqmax=25.00, corners=3, zerophase=False)
+wave1.filter("bandpass", freqmin=0.22, freqmax=24.85, corners=3, zerophase=False)
 wave1.detrend(type='demean')
 wave1.detrend(type='linear')
 wave1.plot(type="dayplot", interval=30, size=(1360, 1060), dpi=164, number_of_ticks=6, tick_rotation=45,
@@ -23,10 +23,10 @@ wave1.plot(type="dayplot", interval=30, size=(1360, 1060), dpi=164, number_of_ti
         color=['k', 'r', 'b', 'g'], show_y_UTC_label=True, outfile='public/waveforms/RO/RO_12H_MLR_HHZ.png')
 """
 
-wave1.filter("bandpass", freqmin=0.09, freqmax=25.00, corners=2, zerophase=False)
+wave1.filter("bandpass", freqmin=0.19, freqmax=25.90, corners=2, zerophase=False)
 wave1.detrend(type='demean')
 wave1.detrend(type='linear')
 wave1.plot(type="dayplot", interval=30, size=(1360, 1060), dpi=170, number_of_ticks=6, tick_rotation=45,
         right_vertical_labels=False,
-        vertical_scaling_range=5e2, one_tick_per_line=True,
+        vertical_scaling_range=5e3, one_tick_per_line=True,
         color=['k', 'r', 'b', 'g'], show_y_UTC_label=True, outfile='public/waveforms/RO/RO_12H_MLR_HHZ.png')
